cb385a9394ba19691a7483474e39a3612b0a1d64
Gabriel_Noleto <gabrielsiqueirarn@gmail.com>
2026-01-20 13:19:52 -0300

    commit inicial

---
+++ src/test/java/com/NoletoTech/bffagendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoletoTech.bffagendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
